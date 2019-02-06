0ea5311f7091a62dfed880f9d248fff5032d4d86
brianw13 <brian.walsh@emc.com>
2019-02-06 18:58:02 -0500

    Initial commit

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackApplication.java
@@ -0,0 +1,11 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackApplication.class, args);
+    }
+}
